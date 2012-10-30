9acf5e12940fd2476a9363b17f7a5a50be29f95c
Michael Graff <explorer@flame.org>
2012-10-30 18:57:07 -0500

    switch to specs

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '84d42ade634c731d426ea5f53613b1f5b7c1ec7164d787e3a00fa40da2350bef019148fbc0e7f997b3daa3af1a3ef9e6b76e62a1e4a74cb7965c0bf091c962ea'
---
+++ spec/gryphon_sitemap_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GryphonSitemapTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, GryphonSitemap
+  end
+end
