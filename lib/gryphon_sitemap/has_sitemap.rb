9a2f72d02de92c20971fefa37b870a3f61ab2eb9
Michael Graff <explorer@flame.org>
2012-10-30 22:51:46 -0500

    refactor out accessors to attr_reader

--- lib/gryphon_sitemap/has_sitemap.rb
+++ lib/gryphon_sitemap/has_sitemap.rb
@@ -1,5 +1,8 @@
 module GryphonSitemap
   module HasSitemap
+    attr_reader :sitemap_size
+    attr_reader :sitemap_fields
+
     def has_sitemap(options = {})
       @sitemap_size = options[:per_page] || 5000
       @sitemap_fields = Array(options[:fields] || [])
@@ -10,23 +13,15 @@ module GryphonSitemap
     module ClassMethods
       def sitemap_page_count
         item_count = count
-        pages = item_count / @sitemap_size
-        pages += 1 if (item_count % @sitemap_size) != 0
+        pages = item_count / sitemap_size
+        pages += 1 if (item_count % sitemap_size) != 0
         pages
       end
 
       def sitemap_page(page)
-        fields = [:id, :updated_at] + @sitemap_fields
+        fields = [:id, :updated_at] + sitemap_fields
 
-        query = order(:id).limit(@sitemap_size).select(fields).offset(page * @sitemap_size)
-      end
-    
-      def sitemap_size
-        @sitemap_size
-      end
-    
-      def sitemap_fields
-        @sitemap_fields
+        query = order(:id).limit(sitemap_size).select(fields).offset(page * sitemap_size)
       end
     end
   end
